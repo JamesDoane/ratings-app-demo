3e22a23cdc7387e204793e5f973ed1e336d8f9fb
JamesDoane <ijamesdoaneok@gmail.com>
2021-09-16 17:05:04 -0500

    this battle station is fully operational

--- ratings.py
+++ ratings.py
@@ -1,13 +1,17 @@
 
 # put your code here
-
+import random
+import os
 
 ratings_dictionary = {}
 menu=''
 
 def read_ratings():
     #open the file
-    ratings_file = open('./scores.txt', 'r')
+    global ratings_dictionary
+    ratings_dictionary = {}
+    filename = input("what file would you like to read ratings from?")
+    ratings_file = open(filename, 'r')
 
     # for loop to loop over each line in the open file, rstrip to take each line without taking 
     # the next, and split it at the semi-colon,removing the semi-colon from the results.
@@ -26,7 +30,8 @@ def print_ratings():
     # good practice to close the file, so yeah.
     
 def user_rating():
-    
+    print("\n")
+
     restaraunt_rated = input("What restaraunt are you reviewing?\n")
     restaraunt_rating = int(input("And how many stars, out of 5, would you rate the restaraunt?\n"))
 
@@ -41,21 +46,72 @@ def user_rating():
         user_rating()
 
 def selection():
-    menu = input("""Welcome to rating restaraunts. enter 1 to retrieve the restaraunts and their ratings. enter 2 to add
-     a restaraunt. enter 3 to exit the program. \n""")
+    counter = 0
+    if counter == 0:
+        menu = input("""Welcome to rating restaraunts. enter 1 to retrieve the restaraunts and their ratings. enter 2 to add
+        a restaraunt. enter 3 to update a random rating. enter 4 to update a specific rating. 
+        enter 5 to list files. enter 6 to import file data. enter 0 to exit the program.\n\n""")
+        counter += 1
     return menu
+
+def random_update():
+    print("\n")
+    to_update = random.choice(list(ratings_dictionary.items()))
+    print(to_update[0] + " has a rating of " + to_update[1])
+    new_rating = int(input("What should it's rating be instead?\n"))
+
+    while new_rating < 1 and new_rating > 5 or None:
+        new_rating = int(input("No seriously, out of 5, what would you rate the restaraunt?\n"))
+
+    ratings_dictionary[to_update[0]] = new_rating
+
+
+def update_rating():
+    print("\n")
+    to_update = input("Which restaurant would you like to update?\n")
+    if ratings_dictionary.get(to_update):
+
+        new_rating = int(input("What should it's rating be instead?\n"))
+
+        while new_rating < 1 and new_rating > 5 or None:
+            new_rating = int(input("No seriously, out of 5, what would you rate the restaraunt?\n"))
     
+        ratings_dictionary[to_update] = new_rating
+
+def list_directory():
+    dir_list = os.listdir()
+    for file in sorted(dir_list):
+        if file.endswith(".txt"):
+            print("\n" + file + "\n")
+
 def main():
     menu = selection()
-    read_ratings()
-    while menu != '3':
+    while menu != '0':
         if menu == '1':
+            print("\n")
             print_ratings()
             menu = selection()
         elif menu == '2':
+            print("\n")
             user_rating()
+            menu = selection()
+        elif menu == '3':
+            print("\n")
+            random_update()
+            menu = selection()
+        elif menu == '4':
+            print("\n")
+            update_rating()
+            menu = selection()
+        elif menu == '5':
+            print("\n")
+            list_directory()
+            menu = selection()
+        elif menu == '6':
+            read_ratings()
             menu = selection()
         else:
             menu = selection()
+
 main()
 
